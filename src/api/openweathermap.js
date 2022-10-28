7068108dcb83fd8cab615c7769b0083cf16c949c
kizey <ikizey@icloud.com>
2022-10-29 02:51:38 +0400

    fix critical bug lat and lon were switched

--- src/api/openweathermap.js
+++ src/api/openweathermap.js
@@ -2,8 +2,8 @@ export const ow_config = (longitude, latitude) => ({
     method: "GET",
     url: "https://api.openweathermap.org/data/2.5/weather",
     params: {
-        lat: `${longitude}`, //required
-        lon: `${latitude}`, //required
+        lat: `${latitude}`, //required
+        lon: `${longitude}`, //required
         appid: process.env.REACT_APP_OPEN_WEATHER_API_TOKEN,
         units: "metric", //optional
     },
