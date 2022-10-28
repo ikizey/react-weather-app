87394c203036e2bd6309a1e7982d6e981451f274
kizey <ikizey@icloud.com>
2022-10-28 19:54:19 +0400

    add Card component add background and city styling

--- src/components/City.js
+++ src/components/City.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Card from "./UI/Card";
 
 const City = ({ city, onCitySelect }) => {
     const clickHandler = e => {
         e.preventDefault();
         onCitySelect(city);
     };
-    return <p onClick={clickHandler}>{city.city}</p>;
+    return (
+        <Card>
+            <p onClick={clickHandler}>{city.city}</p>
+        </Card>
+    );
 };
 
 export default City;
