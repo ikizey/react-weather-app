b1462d6fd852b7a91fbe581cfeb041d951f61f13
kizey <ikizey@icloud.com>
2022-10-28 13:56:45 +0400

    add types filter to geodbapi

--- src/api/geoDB.js
+++ src/api/geoDB.js
@@ -3,7 +3,11 @@ const minPopulation = 120_000;
 export const geodb_config = city => ({
     method: "GET",
     url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
-    params: { minPopulation: `${minPopulation}`, namePrefix: city },
+    params: {
+        minPopulation: `${minPopulation}`,
+        namePrefix: city,
+        types: "CITY",
+    },
     headers: {
         "X-RapidAPI-Key": `${process.env.REACT_APP_GEO_DB_API_TOKEN}`,
         "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
